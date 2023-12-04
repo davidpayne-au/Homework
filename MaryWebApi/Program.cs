3b2f0b93ab2ca6ddad9e82263d77603e60c9992a
Mary Inshala <53507300+inshala@users.noreply.github.com>
2023-12-04 11:14:55 +0530

    Update Program.cs
    add_addup_function

--- MaryWebApi/Program.cs
+++ MaryWebApi/Program.cs
@@ -1,29 +1,44 @@
+// Importing the MaryLib namespace
+using MaryLib;
+
+// Creating a web application builder
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// Adding a scoped service to the dependency injection container
+builder.Services.AddScoped<IService, Service>();
+
+// Adding controllers to the service collection
+builder.Services.AddControllers();
+
+// Adding API explorer services to the service collection
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Building the web application
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configuring the HTTP request pipeline
 if (app.Environment.IsDevelopment())
 {
+    // Enabling Swagger and Swagger UI in the development environment
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+// Enabling HTTPS redirection
 app.UseHttpsRedirection();
 
+// Array of weather summaries
 var summaries = new[]
 {
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
+// Mapping a GET request to the "/weatherforecast" endpoint
 app.MapGet("/weatherforecast", () =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
+    // Generating a weather forecast using LINQ
+    var forecast = Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
         (
             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
@@ -33,14 +48,19 @@ app.MapGet("/weatherforecast", () =>
         .ToArray();
     return forecast;
 })
+// Assigning a name to the endpoint and enabling OpenAPI documentation
 .WithName("GetWeatherForecast")
 .WithOpenApi();
 
-// Have another endpoint here which returns the add up numbers
+// Mapping another GET request to the "/api/calculator/add/{a}/{b}" endpoint
+app.MapGet("/api/calculator/add/{a}/{b}", (int a, int b) => $"{a + b}");
 
+// Running the web application
 app.Run();
 
+// Defining a record type for WeatherForecast
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 {
+    // Calculating temperature in Fahrenheit
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
